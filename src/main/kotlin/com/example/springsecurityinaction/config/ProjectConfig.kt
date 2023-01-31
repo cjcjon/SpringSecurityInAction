4dbc36cf901916cba063dfe3d533a0ed38bacc01
JoonSu Choi <cjcjon@naver.com>
2023-02-01 01:23:26 +0900

    feat: 기초적인 보안 어댑터 상속

--- src/main/kotlin/com/example/springsecurityinaction/config/ProjectConfig.kt
+++ src/main/kotlin/com/example/springsecurityinaction/config/ProjectConfig.kt
@@ -2,6 +2,8 @@ package com.example.springsecurityinaction.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
@@ -9,10 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
-class ProjectConfig {
+class ProjectConfig: WebSecurityConfigurerAdapter() {
 
     @Bean
-    fun userDetailsService(): UserDetailsService {
+    override fun userDetailsService(): UserDetailsService {
         val userDetailsService = InMemoryUserDetailsManager()
 
         val user = User.withUsername("cjcjon").password("12345").authorities("read").build()
@@ -24,5 +26,10 @@ class ProjectConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return NoOpPasswordEncoder.getInstance()
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http.httpBasic()
+        http.authorizeRequests().anyRequest().authenticated()
     }
 }
